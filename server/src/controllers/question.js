33846e9c45b2583ed92d162160bacb4c099e4d45
basilcea <ogbonnabasilcea@gmail.com>
2018-08-23 07:15:00 +0100

    feat(sever): create get a single question endpiont [Finishes #159795515]

--- server/src/controllers/question.js
+++ server/src/controllers/question.js
@@ -1,11 +1,26 @@
 import questions from '../models/question';
 
-const getAllQuestions = (req, res) => {
+export const getAllQuestions = (req, res) => {
   res.status(200).send({
     success: 'true',
     message: 'questions retrieved successfully',
     questions
   });
 };
+export const getAQuestion = (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  questions.map((question) => {
+    if (question.id === id) {
+        res.status(200).send({
+        success: 'true',
+        message: 'question retrieved successfully',
+        question
+      });
+    }
+      return res.status(404).send({
+      success:  'false',
+      message : 'question does not exist'
+    });
+  });
+};
 
-export default getAllQuestions;
--- server/src/routers/index.js
+++ server/src/routers/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import getAllQuestions from '../controllers/question';
+import {getAllQuestions, getAQuestion} from '../controllers/question';
 
 const router = express.Router();
 
 router.get('/questions', getAllQuestions);
+router.get('/questions/:id', getAQuestion);
 
 export default router;
