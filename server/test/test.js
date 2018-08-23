2b70d7265404943b3cf52ffe0040cacaa445430d
basilcea <ogbonnabasilcea@gmail.com>
2018-08-23 12:26:35 +0100

    ch(server): test all endpionts

--- server/test/test.js
+++ server/test/test.js
@@ -2,19 +2,49 @@ import {expect} from 'chai';
 import request from "supertest";
 import app from '../app';
 
-const should = chai.should()
-
-// Test status
-
 describe('Test for question', () => {
   it('get all questions', (done) => {
     request(app)
       .get('/api/v1/questions')
       .end((err, res) => {
         expect(res.status).to.equal(200);
-        res.body.error.should.equal(false)
       });
       done();
   });
+  it('get single questions',(done) =>{
+    request(app)
+    .get('/api/v1/questions/1')
+    .end((err,res) =>{
+      expect(res.status).to.equal(200);
+    });
+    done();
+  });
+  it('post questions', (done) =>{
+    request(app)
+    .post('/api/v1/questions')
+    .send({
+      subject:"subject",
+      description:"description",
+      author:"name"
+    })
+    .end((err,res) =>{
+      expect(res.status).to.equal(201);
+    });
+    done()
+  });
+});
 
+describe('Test for answers', () =>{
+  it('post answers',(done) =>{
+    request(app)
+    .post('/api/v1/questions/1/answers')
+    .send({
+      description:'description',
+      author: 'name'
+  })
+    .end((err,res) =>{
+      expect(res.status).to.equal(201);
+    });
+    done()
+  });
 });
