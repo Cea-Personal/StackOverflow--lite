25dde1bc92ab43c0283bcefd19e470eeb72eccd3
basilcea <ogbonnabasilcea@gmail.com>
2018-08-23 12:19:36 +0100

    Revert "feat(server): create get all questions endpiont"

--- server/src/routers/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import getAllQuestions from '../controllers/question';
-
-const router = express.Router();
-
-router.get('/questions', getAllQuestions);
-
-export default router;
